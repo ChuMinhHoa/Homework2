f152da66caf41e70063e2b1c8f6d98725036bd1a
PinkPig592002 <hoacmgch18105@gmail.com>
2021-01-23 17:49:00 +0700

    Create app3.js

---
+++ app3.js
@@ -0,0 +1,22 @@
+// Web components là gì?
+// hiểu đơn giản thì web components giống như một kịch bản được xây dựng và đóng gói sẵn. Nhằm mục đích tái sử dụng 
+// ở nhiều trường hợp.
+
+
+// Tại sao cần có web component?
+// Khi sử dụng web component chúng ta sẽ dễ dàng chỉnh sửa cập nhật cho trang web của mình.
+// cái này e cũng chỉ hiểu sơ sơ là nó giúp cái mã code của mình trông ngắn gọn và dễ nhìn hơn
+// nó sẽ dễ khiến cho mình dễ nhìn ra lỗi sai nếu có 
+
+
+// Công nghệ
+
+// Custom Elements: là tập hợp các API của Javascript để cho phép tạo ra các thành phần web tuỳ chỉnh,
+// chẳng hạn như thẻ <my-todos> ở ví dụ trên.
+
+// Shadow DOM: là tập hợp các API của Javascript để gắn một cây DOM “shadow” vào một thành phần của DOM,
+// cây DOM này được quản lý riêng biệt và render riêng với cây DOM chính, do vậy nó có thể được đóng gói
+// lại để sử dụng cho các ứng dụng khác nhau.
+
+// HTML templates: Dùng để tạo ra các template từ đó được render ra trang web. Khái niệm này tương đối
+// giống với một số framework khác như angular hay react.js
